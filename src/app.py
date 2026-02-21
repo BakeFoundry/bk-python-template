7cfed245d0b6ece234427c3a300d17589b14194b
PadarbindaPuhan <padarbinda.puhan@gmail.com>
2026-02-21 11:48:59 +0530

    fix: test stat scan again

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! ok this should work</p>"
+    return "<p>Hello, World! ok this should work now</p>"
 
 
 if __name__ == "__main__":
