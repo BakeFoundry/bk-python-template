0d5fa460ad2f89017125d1d52d44a34a06fb7223
PadarbindaPuhan <padarbinda.puhan@gmail.com>
2026-03-08 18:33:41 +0530

    feat: test the code for version tag

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!!!fetch AMI id</p>"
+    return "<p>Hello, World!!!fetch AMI id, ok lets make some change</p>"
 
 
 if __name__ == "__main__":
