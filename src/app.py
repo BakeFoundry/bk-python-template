5c2abcda23834a461fed51466e0f6003b0254c5a
Dilip <dilip.samanta@gmail.com>
2026-03-12 18:03:22 +0530

    feat: fix the changes

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!!!fetch AMI id, ok lets make change</p>"
+    return "<p>Hello, World!!!fetch AMI id, ok lets make some changes</p>"
 
 
 if __name__ == "__main__":
